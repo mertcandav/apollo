d3e83e7621cfb696a233e26b95769ae8745e905f
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-04-11 13:21:37 +0300

    Add isadmin command.

--- src/commands/admin.js
+++ src/commands/admin.js
@@ -7,27 +7,49 @@ const fs = require("fs")
 
 module.exports = {
     process: function(msg) {
-        mval = msg.content.substring(1).toLowerCase();
+        mval = msg.content.substring(1).toLowerCase()
         if(mval == "admins") {
-            admins(msg.channel);
+            admins(msg.channel)
         } else if(mval.startsWith("setadmin ")) {
-            cache = mval.substring(12);
-            serverjson.admins.push(cache.substring(0,cache.length-1));
-            jsonval = JSON.stringify(serverjson);
-            fs.writeFile("./jsonbase/server.json",jsonval,(err) => { });
+            setadmin(mval)
         } else if(mval.startsWith("removeadmin ")) {
-            cache = mval.substring(12);
-            delete serverjson.admins.pop(cache.substring(0,cache.length-1));
-            jsonval = JSON.stringify(serverjson);
-            fs.writeFile("./jsonbase/server.json",jsonval,(err) => { });
+            removeadmin(mval)
+        } else if(mval.startsWith("isadmin ")) {
+            cache = mval.substring(11)
+            cache = cache.substring(0,cache.length-1)
+            msg.channel.send("<@!" + cache + "> is " + (isadmin(cache) ? "admin!" : "not admin!"));
         }
     }
 }
 
+function isadmin(id) {
+    return serverjson.admins.indexOf(id) != -1
+}
+
+function setadmin(mval) {
+    cache = mval.substring(12)
+    cache = cache.substring(0,cache.length-1)
+    if(isadmin(cache) == true)
+        return
+    serverjson.admins.push(cache)
+    jsonval = JSON.stringify(serverjson)
+    fs.writeFile("./jsonbase/server.json",jsonval,(err) => { })
+}
+
+function removeadmin(mval) {
+    cache = mval.substring(12)
+    cache = cache.substring(0,cache.length-1)
+    if(isadmin(cache) == true)
+        return;
+    delete serverjson.admins.pop(cache)
+    jsonval = JSON.stringify(serverjson)
+    fs.writeFile("./jsonbase/server.json",jsonval,(err) => { })
+}
+
 function admins(channel) {
-    let val = "";
+    let val = ""
     all = serverjson.admins.forEach((key) => {
-        val += "<@!" + key + ">\n";
+        val += "<@!" + key + ">\n"
     })
-    channel.send(val);
+    channel.send(val)
 }
