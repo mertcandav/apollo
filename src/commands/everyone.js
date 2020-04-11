26485c94164611394d6d98d1cec03590d44d0477
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-04-11 19:19:50 +0300

    Add developer commands.

--- src/commands/everyone.js
+++ src/commands/everyone.js
@@ -1,6 +1,7 @@
 //#region require
 
 const corejs = require("../engine/core.js");
+const botjson = require("../../jsonbase/bot.json")
 const serverjson = require("../../jsonbase/server.json")
 
 //#endregion
@@ -8,7 +9,13 @@ const serverjson = require("../../jsonbase/server.json")
 module.exports = {
     process: function(msg) {
         mval = corejs.cleanCommand(msg.content)
-        if(mval == "random") {
+        if (mval == "author" || mval == "developer") {
+            msg.reply("Author is " + botjson.info.author + "\nGitHub: " + botjson.info.authorgituri)
+            return true
+        } else if(mval == "repostory" || mval == "repo" || mval == "gitrepo") {
+            msg.reply("Git repostory link;\n" + botjson.info.gituri)
+            return true
+        } else if(mval == "random") {
             msg.reply(corejs.random(100).toString())
             return true
         } else if(mval.startsWith("random ")) {
